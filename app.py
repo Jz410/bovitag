284b8632d19275a38276c9a1d0adc574469fb281
JADELKA / JDT <166043734+NioyJadelkaFp@users.noreply.github.com>
2024-11-28 16:27:52 -0300

    Login Responsivo

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/login')
+def index():
+    return redirect(url_for('inicio'))
+
+@app.route('/login', methods=["POST", "GET"])
 def inicio():
     titulo:str = 'Login'
+    if request.method == "POST":
+        user = request.form.get("user", "N/A")
+        password = request.form.get("password", "N/A")
+
+        if (user, password) == (user,password):
+            pass
+    else:
+        user, password = None, None
     
     return render_template('login.html', titulo=titulo)
 
